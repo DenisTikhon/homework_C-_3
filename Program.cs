1cea9393431410b487ad1b4fec5a4051d9e229ff
DenisT <denis@tikhonov.pp.ru>
2022-08-18 16:26:39 +0300

    add task21

--- Program.cs
+++ Program.cs
@@ -7,3 +7,23 @@ if (poli.Length == 5) {
     Console.WriteLine("Да");
 } else {Console.WriteLine("Нет");}
 } else {Console.WriteLine("Строго пятизначное число");}
+//Задача 21
+Double x1, x2, y1, y2, z1, z2, distance;
+Console.WriteLine("Введите координаты");
+Console.WriteLine("точка A");
+Console.WriteLine("X");
+x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Y");
+y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Z");
+z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты");
+Console.WriteLine("точка B");
+Console.WriteLine("X");
+x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Y");
+y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Z");
+z2 = Convert.ToDouble(Console.ReadLine());
+distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)+ Math.Pow(z2 - z1, 2));
+Console.WriteLine(distance);
