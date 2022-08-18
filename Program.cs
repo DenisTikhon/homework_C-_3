038b69dc2ec773abf4da2fc0b4de53bbd92c4357
DenisT <denis@tikhonov.pp.ru>
2022-08-18 15:42:40 +0300

    add task19

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача 19
+Console.WriteLine("Введите пятизначное число");
+int A = Convert.ToInt32(Console.ReadLine());
+string poli = Convert.ToString(A);
+if (poli.Length == 5) { 
+    if (poli[0].ToString() + poli[1].ToString() == poli[4].ToString() + poli[3].ToString()){
+    Console.WriteLine("Да");
+} else {Console.WriteLine("Нет");}
+} else {Console.WriteLine("Строго пятизначное число");}
